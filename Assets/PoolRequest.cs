cce11dc0f074760051e339be0026a3c484cd004a
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-01-20 13:37:27 +0100

    Work Commit 1
    Added primary structure
    Main functionality not done yet
    Still not sure on how to properly do the requests

---
+++ Assets/PoolRequest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolRequest
+{
+	public readonly string identifier;
+	public readonly int numToPool;
+	public readonly float maxPoolingTime;
+	public bool Prioretize
+	{
+		get
+		{
+			return maxPoolingTime == 0f;
+		}
+	}
+
+	public PoolRequest( string identifier, int numToPool, float maxPoolingTime )
+	{
+		this.identifier = identifier;
+		this.numToPool = numToPool;
+		this.maxPoolingTime = Time.time + maxPoolingTime;
+	}
+
+	public PoolRequest( string identifier, int numToPool )
+	{
+		this.identifier = identifier;
+		this.numToPool = numToPool;
+		maxPoolingTime = 0f;
+	}
+
+	public PoolRequest( string identifier )
+	{
+		this.identifier = identifier;
+		numToPool = 1;
+		maxPoolingTime = 0f;
+	}
+}
---
+++ Assets/Pooler.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler: MonoBehaviour
+{
+
+	public int score;
+
+	private Queue<PoolRequest> toDo;
+	private Queue<PoolRequest> requests;
+	private PoolRequest request;
+	public Dictionary<string, GameObject> PoolingReferences;
+	public Dictionary<string, List<GameObject>> pool;
+
+	public static Pooler curr;
+
+	private void Awake()
+	{
+		if ( curr != null )
+			throw new System.Exception( "Cannot have multiple instances of Pooler" );
+		curr = this;
+		toDo = new Queue<PoolRequest>();
+		requests = new Queue<PoolRequest>();
+	}
+
+
+	private void ProcessPool()
+	{
+		if ( request == null )
+		{
+			int more = toDo.Count;
+			if ( more == 0 )
+			{
+				if ( requests.Count == 0 )
+				{
+					return;
+				}
+				else
+				{
+					toDo.Enqueue( requests.Dequeue() );
+				}
+			}
+
+			// there are requests
+			request = toDo.Dequeue();
+		}
+
+		if ( request.Prioretize )
+		{
+			SpawnObjects( request.identifier, request.numToPool );
+		}
+		else
+		{
+			float timeLeft = request.maxPoolingTime - Time.time;
+			if ( timeLeft < .2f )
+			{
+				SpawnObjects( request.identifier, request.numToPool );
+				return;
+			}
+			int poolNow = request.numToPool;
+			SpawnObjects( request.identifier, poolNow );
+		}
+
+	}
+
+	public void SpawnObjects( string identifier, int count )
+	{
+
+		GameObject go = PoolingReferences[identifier];
+		if ( pool[identifier] == null )
+		{
+			pool.Add( identifier, new List<GameObject>() );
+		}
+		for ( int i = 0; i < count; i++ )
+		{
+			GameObject obj = Instantiate( go );
+			obj.SetActive( false );
+			pool[identifier].Add( obj );
+		}
+	}
+
+	private void Update()
+	{
+		ProcessPool();
+	}
+}
